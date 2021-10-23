29ba10f5df21aa83c995539178a4cd74ed1e2c48
Nakul <nakulbgaikwad1001@gmail.com>
2021-10-23 12:36:47 +0530

    Removed Error From Contact Us page and Added Search Functionality

--- blogger/views.py
+++ blogger/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+
 
 
 from blog.models import blogDb
@@ -10,3 +12,10 @@ def index(request):
     return render(request, 'index.html', context)
 
 
+def search(request):
+    query = request.GET['query']
+    allBlogs = blogDb.objects.filter(title__icontains=query)
+    if allBlogs.count()== 0:
+        messages.warning(request, 'No Search Results Found Please refine Your Query')
+    params = {'allBlogs':allBlogs, 'query':query}
+    return render(request, 'searchResults.html', params)

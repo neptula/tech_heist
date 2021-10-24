177e3f33b3b195d4b738c141c7727b7b471b3f60
Nakul <nakulbgaikwad1001@gmail.com>
2021-10-24 12:51:17 +0530

    Added Comment Section for Blogs

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from blog.models import blogDb
-admin.site.register(blogDb)
+from blog.models import blogDb, blogComment
+
+admin.site.register((blogDb, blogComment))
 
 
 

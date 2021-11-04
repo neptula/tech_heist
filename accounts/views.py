a229919a237addf4c71456e2621b3e4450c59417
Nakul <nakulbgaikwad1001@gmail.com>
2021-11-04 09:43:37 +0530

    added interests field in signup page

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,58 @@
-from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 # Create your views here.
 from . import forms
 from accounts.models import contact
+from accounts.models import userTags
 from django.contrib import messages
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.models import User 
+
+# class SignUp(CreateView):
+#   form_class=forms.SignUpForm
+#   success_url=reverse_lazy('login')
+#   template_name='accounts/signup.html'
 
 
-class SignUp(CreateView):
-  form_class=forms.SignUpForm
-  success_url=reverse_lazy('login')
-  template_name='accounts/signup.html'
+def SignUp(request):
+    if request.method=="POST":
+      # Get the post parameters
+      username=request.POST['username']
+      email=request.POST['email']
+      pass1=request.POST['pass1']
+      pass2=request.POST['pass2']
+      interestingTags=request.POST['tags']
+
+      # check for errorneous input
+      if len(pass1)<8:
+        messages.warning(request, " Your password must contain 8 characters")
+        return redirect('accounts:signup')
+
+      if not username.isalnum():
+        messages.warning(request, " User name should only contain letters and numbers")
+        return redirect('accounts:signup')
+
+      if User.objects.filter(username=username).exists():
+        # messages.error(request,'Choose Unique Title')
+        messages.warning(request, ' Choose Unique username')
+        return redirect('accounts:signup')
+
+
+      if (pass1!= pass2):
+        messages.warning(request, " Passwords do not match")
+        return redirect('accounts:signup')
+
+      
+      # Create the user
+      myuser = User.objects.create_user(username, email, pass1)
+      myuser.save()
+      newusertags = userTags(username=username, interestingTags=interestingTags) 
+      newusertags.save()
+      messages.success(request, " Your iCoder has been successfully created")
+      return redirect('accounts:login')
+    else:
+      return render(request, 'accounts/signup.html')
 
 def contactus(request):
   if request.method == 'POST':

6ccef73bd6a1cc16eeb6662514c5864a64d906e7
Nakul <nakulbgaikwad1001@gmail.com>
2021-11-04 11:21:00 +0530

    Added interesting tags field in signup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from accounts.models import contact
+from accounts.models import userTags
 
 # Register your models here.
 
-admin.site.register(contact)
+admin.site.register((contact,userTags))
---
+++ static/js/signup.js
@@ -0,0 +1,33 @@
+$(function () {
+    $('#selectpickerid').selectpicker();
+
+
+
+  });
+
+
+  $(document).ready(function () {
+
+
+    $('#selectpickerid').on('change', function () {
+      var textDisplay = $('#selectpickerid option:selected').text();
+      $("#hiddenSelected").val(textDisplay);
+    })
+
+  })
+
+
+  $(document).ready(function () {
+
+    var last_valid_selection = null;
+
+    $('#selectpickerid').change(function (event) {
+
+      if ($(this).val().length >= 3) {
+
+        $(this).val(last_valid_selection);
+      } else {
+        last_valid_selection = $(this).val();
+      }
+    });
+  });

ff8f6660bf77d4bb89a8d78cf772675ec7ae5240
Nakul <nakulbgaikwad1001@gmail.com>
2021-11-05 09:58:55 +0530

    added priority view of blogs in blogs page

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,7 @@ class blogDb(models.Model):
     content = RichTextField(blank=True, null=True)
     views = models.IntegerField(default=0)
     tags = models.CharField(max_length=255)
+    blogpriority = models.IntegerField(default=0)
     # content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True,blank=True)
     def __str__(self):
--- blogger/urls.py
+++ blogger/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('posts/', include('posts.urls' ,namespace='posts')),
     path('blog/', include('blog.urls')),
     path('search/', views.search,name='search'),
+    path('blogs', views.blogs,name='blogs'),
 ]
 
  

5e4785cfdfd0a1d4110169e4699cfb293cd3ee7a
madhuri130801 <73709102+madhuri130801@users.noreply.github.com>
2021-11-05 13:37:42 +0530

    updated settings

--- blogger/settings.py
+++ blogger/settings.py
@@ -141,3 +141,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 LOGIN_REDIRECT_URL='/'
 LOGOUT_REDIRECT_URL='/'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'johncena17148@gmail.com'
+EMAIL_HOST_PASSWORD = 'N@KUL@2002'

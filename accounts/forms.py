4e2f5c28fafb42c7076891940da2de473378d9a7
Nandita Rani <rnandita314@gmail.com>
2021-11-07 15:48:04 +0530

    added last_login field in editprofileform and removed redundant code

--- accounts/forms.py
+++ accounts/forms.py
@@ -14,32 +14,7 @@ class editprofileform(UserChangeForm):
     #is_superuser=forms.CharField(max_length=100,widget=forms.CheckboxInput(attrs={'class':'form-check'}))
     #is_staff=forms.CharField(max_length=100,widget=forms.CheckboxInput(attrs={'class':'form-check'}))
     is_active=forms.CharField(max_length=100,widget=forms.CheckboxInput(attrs={'class':'form-check'}))
-    #last_login=forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
+    last_login=forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control'}))
     class Meta:
         model=get_user_model()
         fields=("username","first_name","last_name", "email","is_active")
-
-# from django.contrib.auth import get_user_model
-# from django.contrib.auth.forms import UserCreationForm
-# from django import forms
-
-# class SignUpForm(UserCreationForm):
-#     extra_field=forms.CharField(required=True)
-
-
-#     class Meta:
-#         model = get_user_model()
-#         fields = ("username", "email", "password1", "password2","extra_field", 'image')
-#         widgets={
-#             'image': forms.CharField(max_length=255)
-#         }
-    
-
-
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.fields["username"].label = "Display name"
-#         self.fields["email"].label = "Email address"
-#         self.fields["extra_field"].label = "Interested Topics"
-
-        

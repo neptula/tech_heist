edf2b888149d3ad279f234c70e646865a90cff0d
Nakul <nakulbgaikwad1001@gmail.com>
2021-11-08 19:21:25 +0530

    Added thumbnail section in writeblog page

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from blog.models import blogDb, blogComment
 from blog.templatetags import extras
 from blog.forms import writeBlogForm
 
+
 def editblog(request, slug=None):
   blog = blogDb.objects.get(slug = slug)
   initVal = {
@@ -35,9 +36,10 @@ def writeblog(request):
     author=request.POST.get('author')
     content=request.POST.get('content')
     tags = request.POST.get('tags')
+    getthumbnail = request.FILES['thumbnail']
     tagsArr=tags.split(' ')
     initVal = {'title':title, 'content':content}
-    form = writeBlogForm(initial = initVal)
+    form = writeBlogForm(request.POST, request.FILES, initial = initVal)
     # some checks of form
 
     if blogDb.objects.filter(title=title).exists():
@@ -47,7 +49,7 @@ def writeblog(request):
       return render(request, 'writeBlog.html', context = params)
 
     else:
-      newBlog=blogDb(title=title, author=author, content=content, tags=tags)
+      newBlog=blogDb(title=title, author=author, content=content, tags=tags, thumbnail=getthumbnail)
       newBlog.save()
       blog=blogDb.objects.get(title=title)
       return redirect(f"/blog/showblog/{blog.slug}")
